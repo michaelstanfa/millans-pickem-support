bffd9e7e333d6a06f4c573f431d3a658b2192908
michaelstanfa <stanfa.michael@gmail.com>
2021-10-23 11:04:49 -0500

    adding specific week script

---
+++ scripts/update_specific_week_records.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import requests
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import math
+from datetime import date
+
+FB_ACCOUNT_FILE= os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+MSF_NFL_ID = os.getenv("MSF_NFL_ID")
+MSF_NFL_SECRET= os.getenv("MSF_NFL_SECRET")
+PICKS = ['pick_1', 'pick_2', 'pick_3']
+DAY_0_OF_SEASON = date(2021, 9, 6)
+
+class UpdateSpecificWeekRecords:
+
+    def __init__(self):
+
+        if(len(sys.argv) == 1):
+            print("This script required a week-of-season integer input.")
+            sys.exit(0)
+
+        cred = credentials.Certificate(FB_ACCOUNT_FILE)
+        firebase_admin.initialize_app(cred)
+        db = firestore.client()
+
+        # firebase crap
+        users = db.collection(u'users')
+        weekNumber = sys.argv[1]
+
+        for user in users.get():
+
+            week = users.document(user.id).collection('seasons').document('202122').collection('weeks').document(str(weekNumber)).get()
+            weekRef = users.document(user.id).collection('seasons').document('202122').collection('weeks').document(str(weekNumber))
+
+            for pick in PICKS:
+                print(pick)
+                if(week.get(pick) != None):
+                    thisPick = week.get(pick)
+                    date = thisPick.get('date').replace("-", "")
+                    print(date)
+                    team = thisPick.get('team')
+                    print(team)
+                    response = requests.get(
+                        f'https://api.mysportsfeeds.com/v1.2/pull/nfl/2021-regular/scoreboard.json?fordate={date}&status=final&team={team}',
+                        auth=(MSF_NFL_ID, MSF_NFL_SECRET))
+                    r = response.json()
+
+                    home = True if r['scoreboard']['gameScore'][0]['game']['homeTeam']['Abbreviation'] == team else False
+                    homeScore = float(r['scoreboard']['gameScore'][0]['homeScore'])
+                    line = float(thisPick.get('line'))
+                    awayScore = float(r['scoreboard']['gameScore'][0]['awayScore'])
+                    if(home):
+                        total = homeScore + line - awayScore
+                    else:
+                        total = awayScore + line - homeScore
+                    
+                    print(total)
+                    if total > 0:
+                        result = "W"
+                    else:
+                        result = "L"
+
+                    weekRef.update({f"{pick}.result":f"{result}"})
+
+
+UpdateSpecificWeekRecords()
